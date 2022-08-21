f5d59ea2339e84420fd14cc318810490930dbfbf
timtoronto634 <tim225x@gmail.com>
2022-08-21 23:42:55 +0900

    add 7.7

---
+++ chapter7/Locker.hs
@@ -0,0 +1,29 @@
+import qualified Data.Map as Map
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
+  Nothing -> Left $ "Locker " ++ show lockerNumber
+    ++ " doesn't exist!"
+  Just (state, code) -> if state /= Taken
+                        then Right code
+                        else Left $ "Locker " ++ show lockerNumber
+                                ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+  [(100, (Taken, "ZD391"))
+  ,(101, (Free, "JAH32"))
+  ,(103, (Free, "IQSA9"))
+  ,(105, (Free, "QOTSA"))
+  ,(109, (Taken, "893JJ"))
+  ,(110, (Taken, "99292"))
+  ]
+
+data List a = Empty | Cons (listhead :: a, listTail :: List a)
+  deriving (Show, Read, Eq, Ord)
---
+++ chapter7/Phone.hs
@@ -0,0 +1,9 @@
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+type AssocList k v = [(k, v)]
+type IntMap v = Map Int v
